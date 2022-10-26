767bf367f3db0be08c38568d9cfda3a147dfcc5e
naeemahmed1994 <ahmednaeemnafi@gmail.com>
2022-10-26 18:48:12 +0600

    Added laravel currency converter

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Currency Exchange</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+</head>
+<body class="bg-gray-200 h-screen leading-none font-sans">
+    @yield('content')
+</body>
+</html>
